f23e06a3a2ab11f9198308749eee0446489992ab
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2020-07-30 11:42:05 +0800

    b6i0l5l commit
    home page update

--- src/App.js
+++ src/App.js
@@ -2,48 +2,79 @@ import React, { Component } from "react";
 import { Avatar, Card, Button} from 'antd';
 import { UserOutlined ,SearchOutlined} from '@ant-design/icons';
 import './App.css';
+import 'antd/dist/antd.css';
 
+const { Meta,Grid } = Card;
 
 
 class FoodPlan extends Component{
-
    render() {
     return (
-
       <div class = "Container">
         <div class = "Header">
-          header
+          <Button type="link" class = "ant-btn-link">
+            Home
+          </Button>
+          <Button type="link" class = "ant-btn-link">
+            Homade
+          </Button>
+          <Button type="link" class = "ant-btn-link">
+            Restaurant
+          </Button>
+          <Button type="link" class = "ant-btn-link">
+            Vegetarians
+          </Button>
+          <Button type="link" class = "ant-btn-link">
+            Desserts
+          </Button>
         </div>
-        <div class = "LeftSide">
-          <Avatar shape="square" size={100} icon={<UserOutlined />} style={{display:'flex',backgroundColor:"white",alignItems:'center',justifyContent:'center',marginLeft:"10%"}}/>
-          <div class = "Info">
-            <p>Name: </p>
-            <p>Likes: </p>
-            <p>Posts: </p>
+        <div class = "Test">
+
+        </div>
+        <div class = "Content1">
+        </div>
+        <div class = "Content21">
+          <div class = "Image">
+            <Card
+              class = "ant-card-cover"
+              cover={<img alt="example" src="content2.png" />}
+            >
+              <Meta title="HomeMade" description="www.instagram.com" />
+            </Card>
           </div>
-          <div class = "Button">
-            <Button type="primary"  icon={<SearchOutlined />} style={{width:150,height:40}}>
-              Search
-            </Button>
-            <Button type="primary"  style={{width:150,height:40,marginTop:'10%'}}>
-              HomeMade
-            </Button>
-            <Button type="primary"  style={{width:150,height:40,marginTop:'1%'}}>
-              Restaurant
-            </Button>
-            <Button type="primary"  style={{width:150,height:40,marginTop:'1%'}}>
-              Vegetarian
-            </Button>
-            <Button type="primary"  style={{width:150,height:40,marginTop:'1%'}}>
-              QA
-            </Button>
+        </div>
+        <div class = "Content22">
+          <div class = "Image">
+            <Card
+              class = "ant-card-cover"
+              cover={<img alt="example" src="content2.png" />}
+            >
+              <Meta title="Restaurant" description="www.instagram.com" />
+            </Card>
+          </div>
+        </div>
+        <div class = "Content23">
+          <div class = "Image">
+            <Card
+              class = "ant-card-cover"
+              cover={<img alt="example" src="content2.png" />}
+            >
+              <Meta title="Vegetarians" description="www.instagram.com" />
+            </Card>
+          </div>
+        </div>
+        <div class = "Content24">
+          <div class = "Image">
+            <Card
+              class = "ant-card-cover"
+              cover={<img alt="example" src="content2.png" />}
+            >
+              <Meta title="Europe Street beat" description="www.instagram.com" />
+            </Card>
           </div>
         </div>
         <div class = "Footer">
           Footer
-        </div>
-        <div class = "Body">
-          Body
         </div>
       </div>
 
